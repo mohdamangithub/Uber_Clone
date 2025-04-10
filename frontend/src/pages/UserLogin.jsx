c20c6370eadfcbe238d8c544702673a4deaa701b
mohdamangithub <mansooriaman025@gmail.com>
2025-04-10 18:01:45 +0530

    all complete

--- frontend/src/pages/UserLogin.jsx
+++ frontend/src/pages/UserLogin.jsx
@@ -2,7 +2,7 @@ import React, { useState, useContext } from 'react'
 import { Link } from 'react-router-dom'
 import { UserDataContext } from '../context/UserContext'
 import { useNavigate } from 'react-router-dom'
-import UberTitle from '../assets/images/uberWhite.png'
+import UberTitle from '../assets/images/black.png'
 import axios from 'axios'
 
 const UserLogin = () => {
